0ea91330eb7e3965e802ab6089f9384318d95655
parinitapiplewar <parinitaapiplewar@gmail.com>
2026-07-20 19:09:57 +0530

    improved

---
+++ Backend/generateHash.js
@@ -0,0 +1,11 @@
+const bcrypt = require("bcryptjs");
+
+async function generateHash() {
+    const password = "Admin@123"; // Change this to the password you want
+    const hash = await bcrypt.hash(password, 10);
+
+    console.log("Password:", password);
+    console.log("Hash:", hash);
+}
+
+generateHash();
